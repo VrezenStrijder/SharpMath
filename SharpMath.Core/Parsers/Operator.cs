10c9f870a2109ec27a180fc4e60f9f2f992932a9
VrezenStrijder <f94fbr@outlook.com>
2025-08-19 18:16:04 +0800

    Create Operator.cs

---
+++ SharpMath.Core/Parsers/Operator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace SharpMath.Core
+{
+    /// <summary>
+    /// 运算符
+    /// </summary>
+    public class OperatorDefinition
+    {
+        public string Symbol { get; set; }
+
+        public int Precedence { get; set; }
+
+        public OperatorType Type { get; set; }
+
+        public Func<Expression, Expression, Expression> BinaryOperation { get; set; }
+
+        public Func<Expression, Expression> UnaryOperation { get; set; }
+
+
+    }
+
+    public enum OperatorType
+    {
+        Binary,
+        Unary
+    }
+}
