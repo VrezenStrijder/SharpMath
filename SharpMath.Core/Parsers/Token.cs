f02f64aff7f2650931b8ca9b0d713226c8942dc2
VrezenStrijder <f94fbr@outlook.com>
2025-08-19 18:16:09 +0800

    Create Token.cs

---
+++ SharpMath.Core/Parsers/Token.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpMath.Core
+{
+
+    public class Token
+    {
+        public Token(TokenType type, string value)
+        {
+            Type = type;
+            Value = value;
+        }
+
+        public TokenType Type { get; }
+
+        public string Value { get; }
+
+    }
+
+    public enum TokenType
+    {
+        Number,
+        Variable,
+        Constant,
+        Operator,
+        Function,
+        LeftParen,
+        RightParen,
+        UnaryOperator,      // 取负号
+        Comma               // 逗号
+    }
+
+}
