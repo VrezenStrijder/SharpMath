d975b6bebf5bf5260e56ace56ce953f452885e76
VrezenStrijder <f94fbr@outlook.com>
2025-09-27 08:47:29 +0800

    Create AvalonEditDocumentConverter.cs

---
+++ SharpMath.UI/Converters/AvalonEditDocumentConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ICSharpCode.AvalonEdit.Document;
+using System.Windows.Data;
+
+namespace SharpMath.UI.Converters
+{
+    public class AvalonEditDocumentConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is string s ? new TextDocument(s) : new TextDocument();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is TextDocument doc ? doc.Text : string.Empty;
+        }
+    }
+
+}
