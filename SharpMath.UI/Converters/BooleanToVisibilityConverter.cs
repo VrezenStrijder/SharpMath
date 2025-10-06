452f27a145657dfd57e195f070b0f2531f4d7ec7
VrezenStrijder <f94fbr@outlook.com>
2025-10-06 18:21:01 +0800

    Create BooleanToVisibilityConverter.cs

---
+++ SharpMath.UI/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows;
+
+namespace SharpMath.UI.Converters
+{
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                bool inverse = parameter?.ToString() == "Inverse";
+                bool result = inverse ? !boolValue : boolValue;
+                return result ? Visibility.Visible : Visibility.Collapsed;
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
