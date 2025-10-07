002085f19915c4b1432864eab8a20d5eac7fe8df
VrezenStrijder <f94fbr@outlook.com>
2025-10-07 22:42:16 +0800

    Create VariableViewModel.cs

---
+++ SharpMath.UI/ViewModels/VariableViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SharpMath.UI.ViewModels
+{
+    public partial class VariableViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string name;
+
+        [ObservableProperty]
+        private string value = "0";
+    }
+
+}
