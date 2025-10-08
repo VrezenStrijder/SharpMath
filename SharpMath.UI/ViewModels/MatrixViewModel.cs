ad40127e8d528e61753e6cecde629034c471c8b2
VrezenStrijder <f94fbr@outlook.com>
2025-10-08 21:37:05 +0800

    Create MatrixViewModel.cs

---
+++ SharpMath.UI/ViewModels/MatrixViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using SharpMath.Core;
+
+namespace SharpMath.UI.ViewModels
+{
+    public partial class MatrixViewModel : ObservableObject
+    {
+        [ObservableProperty] 
+        private string name;
+
+        [ObservableProperty] 
+        private string matrixText;
+    }
+
+}
