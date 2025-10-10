dd2fbdb039e342218f763ca0b23cce3a76363269
VrezenStrijder <f94fbr@outlook.com>
2025-10-10 17:55:34 +0800

    Create Enums.cs

---
+++ SharpMath.UI/ViewModels/Enums.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpMath.UI.ViewModels
+{
+    public enum CalculateType
+    {
+        自动检测 = 0,
+        化简表达式,
+        求解方程,
+        求解方程组,
+        矩阵运算
+    }
+
+}
