d09731936e93d415e2766da3350995af2015887b
VrezenStrijder <f94fbr@outlook.com>
2025-10-17 17:55:08 +0800

    Create EquationSystemCase.cs

---
+++ SharpMath.Test/EquationSystemCase.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpMath.Core;
+
+namespace SharpMath.Test
+{
+    public class EquationSystemCase
+    {
+
+        public static void SimpleCase()
+        {
+            IParser parser = new AdvancedMathParser();
+
+            var equations = new List<EquationExpression>
+            {
+                new EquationExpression(
+                    parser.Parse("2*x + 3*y - z"),
+                    parser.Parse("5")
+                ),
+                new EquationExpression(
+                    parser.Parse("x - y + 2*z"),
+                    parser.Parse("5")
+                ),
+                new EquationExpression(
+                    parser.Parse("3*x + y + z"),
+                    parser.Parse("8")
+                )
+            };
+
+            var system = new EquationSystemExpression(equations);
+            var systemSolver = new EquationSystemSolver();
+            var result = systemSolver.Process(system);
+
+            Console.WriteLine(result.AnswerText);
+        }
+    }
+
+
+}
