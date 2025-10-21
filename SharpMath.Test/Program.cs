18d38e6f1f86959ad9ed139e468b7d0b90962fcf
VrezenStrijder <f94fbr@outlook.com>
2025-10-21 09:55:03 +0800

    Create Program.cs

---
+++ SharpMath.Test/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SharpMath.Test;
+
+ImageRecognitionCase.UnifiedOCRCase();
+
+ExpressionCase.Case1();
+ExpressionCase.Case2();
+ExpressionCase.Case3();
+
+EquationSystemCase.SimpleCase();
+
+MatrixCase.SimpleCase();
+MatrixCase.ConvertCase();
